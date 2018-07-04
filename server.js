d02123ea0a58ceb21a0aa688c21c86e0e8b38d7c
livne <livne@coro.net>
2018-07-04 16:17:34 +0000

    second commit

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-test
+// Load the module dependencies
+var express = require('./config/express'),
+    mongoose = require('./config/mongoose');
+
+// create a new Mongoose connection instance
+var db = mongoose();
+// create a new Express application instance
+var app = express();
+// create a http module from express
+var server = require('http').createServer(app);
+// create a new io module as socket io
+var io = socket(server);
+
+// Set The Port
+app.set('port', process.env.PORT || 3000);
+
+// Start the server
+server.listen(app.get('port'), function() {
+    console.log('Express server listening on port '+ server.address().port +' ..');
+});
