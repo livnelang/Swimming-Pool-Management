696a999d30de7f5cc336887eceee6879390f9d01
livne <livne@coro.net>
2018-07-05 07:09:09 +0000

    third commit

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ var express = require('./config/express'),
     mongoose = require('./config/mongoose');
 
 // create a new Mongoose connection instance
-var db = mongoose();
+// var db = mongoose();
+
 // create a new Express application instance
 var app = express();
 // create a http module from express
 var server = require('http').createServer(app);
-// create a new io module as socket io
-var io = socket(server);
 
 // Set The Port
 app.set('port', process.env.PORT || 3000);
