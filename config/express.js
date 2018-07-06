af9cfb87d105810076d6a8c7c82cca446d25e8f1
livne <livne@coro.net>
2018-07-06 10:28:34 +0000

    commit

---
+++ config/express.js
@@ -0,0 +1,31 @@
+//Load the module dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+//Define the express configuration
+module.exports = function() {
+
+    //Create a new express instance
+    var app = express();
+
+    //configure body parser
+    app.set('json spaces',4);
+
+    //Here we are configuring express to use body-parser as middle-ware.
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+
+    //add access control response
+    app.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
+    // Load the routing files
+    require('../app/routes/users.routes.js')(app);
+
+    //Return the Express application instance
+    return app;
+};
---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+// Load the module dependencies
+var mongoose = require('mongoose');
+
+// Define the Mongoose configuration schemas
+module.exports = function() {
+    //Connect to MongoDB with Mongoose
+    var db = mongoose.connect('mongodb://poolAdmin:1a2s3d4f@ds129321.mlab.com:29321/swimming-pool');
+
+    db = mongoose.connection;
+    //Load the application models
+    require('../app/models/users.model');
+
+    //Return the Mongoose connection instance
+    return db;
+};
